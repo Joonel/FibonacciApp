f5806870f3e237f10bec74f9c91c04dcb2d5976f
Olexandr Brazhiy <brazhiyolexandr@gmail.com>
2023-09-30 13:03:46 +0300

    1st commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть кількість чисел Фібоначі для обчислення:");
+        int n = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a + " ");
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+    }
+}
